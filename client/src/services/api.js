e0d170e136e24aa7e1832c797f741b84b4059ef4
SriveniBingi <srivenibingi18@gmail.com>
2026-04-17 22:47:27 +0530

    "Code cleanup"

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -3,10 +3,7 @@ import axios from 'axios';
 // 1. We define the dynamic base. 
 // If VITE_API_URL is set (on Vercel), it uses that.
 // If not (on your Laptop), it uses localhost.
-const BASE_URL = import.meta.env.VITE_API_URL 
-  ? `${import.meta.env.VITE_API_URL}/api` 
-  : 'http://localhost:5000/api';
-
+const BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000';
 // 2. Now your API object is ready for both!
 const API = axios.create({ 
     baseURL: BASE_URL 
