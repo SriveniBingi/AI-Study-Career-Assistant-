b6451f38c099030c880708bc1818a878ea4b36aa
SriveniBingi <srivenibingi18@gmail.com>
2026-04-17 22:05:35 +0530

    "Minor updates"

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -12,4 +12,20 @@ const API = axios.create({
     baseURL: BASE_URL 
 });
 
+export const summarizeNotes = async (formData) => {
+    // This uses your custom API instance we just set up
+    const response = await API.post('/api/summarize', formData, {
+        headers: { 'Content-Type': 'multipart/form-data' }
+    });
+    return response.data;
+};
+
+export const generateRoadmap = async (data) => {
+    const response = await API.post('/api/roadmap', data);
+    return response.data;
+};
+
+export const loginUser = (data) => API.post('/api/auth/login', data);
+export const signupUser = (data) => API.post('/api/auth/signup', data);
+
 export default API;
